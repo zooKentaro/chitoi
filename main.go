55349b2c189f73f8e7979a2bd4e48214cf602f6b
uenoryo <r.r.ag0611@outlook.jp>
2018-10-07 13:46:47 +0900

    load .env file.

--- main.go
+++ main.go
@@ -1,13 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/uenoryo/chitoi/api"
 )
 
 func main() {
+	if os.Getenv("CHITOI_ENV") == "" {
+		os.Setenv("CHITOI_ENV", "development")
+	}
+
+	err := godotenv.Load(fmt.Sprintf(".env.%s", os.Getenv("CHITOI_ENV")))
+	if err != nil {
+		log.Fatal("Error loading .env file")
+		return
+	}
 
 	server := api.NewServer()
 
