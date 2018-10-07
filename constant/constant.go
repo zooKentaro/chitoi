74391fa7e9e705e48d9ca885f5f5247c121c5093
uenoryo <r.r.ag0611@outlook.jp>
2018-10-07 12:40:03 +0900

    add platform type to constant.

---
+++ constant/constant.go
@@ -0,0 +1,13 @@
+package constant
+
+const (
+
+	// PlatformTypePC は プラットフォームが pc であることを表す
+	PlatformTypePC = 1
+
+	// PlatformTypeIOS は プラットフォームが ios であることを表す
+	PlatformTypeIOS = 2
+
+	// PlatformTypeAndroid は プラットフォームが android であることを表す
+	PlatformTypeAndroid = 3
+)
