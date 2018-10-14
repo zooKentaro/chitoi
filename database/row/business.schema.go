3acdb36d3b22a5ab386521e2bb8e365c1dd96628
uenoryo <r.r.ag0611@outlook.jp>
2018-10-14 18:56:00 +0900

    add db tag to schema

--- database/row/business.schema.go
+++ database/row/business.schema.go
@@ -9,12 +9,12 @@ type Business struct {
     ID               uint64 `json:"id"`
     Prefecture       uint32 `json:"prefecture"`
     Name             string `json:"name"`
-    PriceBase        uint32 `json:"price_base"`
-    PriceLevel2      uint32 `json:"price_level2"`
-    PriceLevel3      uint32 `json:"price_level3"`
-    ReturnRateBase   uint32 `json:"return_rate_base"`
-    ReturnRateLevel2 uint32 `json:"return_rate_level2"`
-    ReturnRateLevel3 uint32 `json:"return_rate_level3"`
+    PriceBase        uint32 `db:"price_base" json:"price_base"`
+    PriceLevel2      uint32 `db:"price_level2" json:"price_level2"`
+    PriceLevel3      uint32 `db:"price_level3" json:"price_level3"`
+    ReturnRateBase   uint32 `db:"return_rate_base" json:"return_rate_base"`
+    ReturnRateLevel2 uint32 `db:"return_rate_level2" json:"return_rate_level2"`
+    ReturnRateLevel3 uint32 `db:"return_rate_level3" json:"return_rate_level3"`
 }
 
 func (b Business) PrimaryKey() dialect.PrimaryKey {
