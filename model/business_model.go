8fc8c4e36fc98d95ab4fd1f865e3305f8f478cad
uenoryo <r.r.ag0611@outlook.jp>
2018-10-14 19:07:12 +0900

    businessのラインナップをランダムにする.

--- model/business_model.go
+++ model/business_model.go
@@ -2,6 +2,8 @@ package model
 
 import (
     "database/sql"
+    "math/rand"
+    "time"
 
     "github.com/pkg/errors"
     "github.com/uenoryo/chitoi/core"
@@ -17,12 +19,25 @@ func NewBusinessRepository(core *core.Core) *BusinessRepository {
 }
 
 func (repo *BusinessRepository) TodaysBusiness() ([]*row.Business, error) {
+    prefNum, err := repo.todaysPrefNum()
+    if err != nil {
+        return nil, errors.Wrap(err, "error today's pref num")
+    }
+
     businessRows := []*row.Business{}
-    if err := repo.core.DB.Select(&businessRows, "SELECT * FROM business WHERE prefecture = ?", 43); err != nil {
+    if err := repo.core.DB.Select(&businessRows, "SELECT * FROM business WHERE prefecture = ?", prefNum); err != nil {
         if err == sql.ErrNoRows {
             return nil, errors.Wrap(err, "business is not found")
         }
         return nil, err
     }
     return businessRows, nil
+}
+
+// todaysPrefNum は今日の都道府県番号を返す (日替わりのランダム)
+func (repo *BusinessRepository) todaysPrefNum() (uint32, error) {
+    rand.Seed(time.Now().UnixNano())
+    num := uint32(rand.Intn(8) + 40) // TODO: 現状は40 ~ 47 まで。マスターが入ったら 1 ~ 47にする
+
+    return num, nil
 }
