3300abd4e79530e1bbaf5fd8b0876921a1f7d1fd
uenoryo <r.r.ag0611@outlook.jp>
2018-10-14 23:12:27 +0900

    add user_business schema

---
+++ database/row/user_business.schema.go
@@ -0,0 +1,27 @@
+package row
+
+import (
+    "time"
+
+    "github.com/kayac/ddl-maker/dialect"
+    "github.com/kayac/ddl-maker/dialect/mysql"
+)
+
+type UserBusiness struct {
+    ID         uint64    `json:"id"`
+    UserID     uint64    `db:"user_id" json:"user_id"`
+    BusinessID uint64    `db:"business_id" json:"business_id"`
+    Level      uint32    `db:"level" json:"level"`
+    LastBuyAt  time.Time `db:"last_buy_at" json:"last_buy_at"`
+}
+
+func (r UserBusiness) PrimaryKey() dialect.PrimaryKey {
+    return mysql.AddPrimaryKey("id")
+}
+
+// Indexes is XXX
+func (r *UserBusiness) Indexes() dialect.Indexes {
+    return dialect.Indexes{
+        mysql.AddUniqueIndex("user_business_idx", "user_id", "business_id"),
+    }
+}
