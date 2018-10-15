72dd07f1e1cd21619bf2e0a736bda3365b87e93d
uenoryo <r.r.ag0611@outlook.jp>
2018-10-16 00:48:51 +0900

    masterdataをメモリに乗せる.

--- core/core.go
+++ core/core.go
@@ -1,16 +1,24 @@
 package core
 
 import (
+    "database/sql"
+
     redigo "github.com/garyburd/redigo/redis"
     "github.com/jmoiron/sqlx"
     "github.com/pkg/errors"
     "github.com/uenoryo/chitoi/database"
+    "github.com/uenoryo/chitoi/database/row"
     "github.com/uenoryo/chitoi/redis"
 )
 
 type Core struct {
-    DB    *sqlx.DB
-    Redis redigo.Conn
+    DB         *sqlx.DB
+    Redis      redigo.Conn
+    Masterdata Masterdata
+}
+
+type Masterdata struct {
+    Businesses []*row.Business
 }
 
 func New() (*Core, error) {
@@ -28,4 +36,16 @@ func New() (*Core, error) {
         DB:    dbConn,
         Redis: redisConn,
     }, nil
+}
+
+func (core *Core) LoadMasterdata() error {
+    businesses := []*row.Business{}
+    if err := core.DB.Select(&businesses, "SELECT * FROM business"); err != nil {
+        if err != sql.ErrNoRows {
+            return errors.Wrap(err, "error select all business")
+        }
+    }
+
+    core.Masterdata.Businesses = businesses
+    return nil
 }
