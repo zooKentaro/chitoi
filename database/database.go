1c0e7d513a8a1d5bd0e9b28c06b77cf2d729509c
uenoryo <r.r.ag0611@outlook.jp>
2018-10-18 00:40:09 +0900

    created_atをJSTにする.

--- database/database.go
+++ database/database.go
@@ -12,7 +12,7 @@ func Connect() (*sqlx.DB, error) {
 	return sqlx.Connect(
 		"mysql",
 		fmt.Sprintf(
-			"%s:%s@tcp(%s:%s)/%s?parseTime=true",
+			"%s:%s@tcp(%s:%s)/%s?parseTime=true&loc=Asia%%2FTokyo",
 			os.Getenv("CHITOI_DB_USER"),
 			os.Getenv("CHITOI_DB_PASS"),
 			os.Getenv("CHITOI_DB_HOST"),
