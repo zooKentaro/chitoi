ec9a2c14c165cab8029024b6d2606ba2bea1e269
uenoryo <uenoryo@uma-b.com>
2019-08-14 23:45:12 +0900

    add json tag.

--- websocket/packet/den.go
+++ websocket/packet/den.go
@@ -2,16 +2,16 @@ package packet
 
 // Card はトランプのカード
 type Card struct {
-	ID     uint32
-	Mark   uint32
-	Number uint32
+	ID     uint32 `json:"id"`
+	Mark   uint32 `json:"mark"`
+	Number uint32 `json:"number"`
 }
 
 // Deck は山札
 type Deck struct {
-	Cards []*Card
+	Cards []*Card `json:"cards"`
 }
 
 type TurnTable struct {
-	PlayerIDs []uint32
+	PlayerIDs []uint32 `json:"player_ids"`
 }
