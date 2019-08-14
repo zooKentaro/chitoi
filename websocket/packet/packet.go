85fec3867884acaca1fd9e94188bfa4d3e074efe
uenoryo <uenoryo@uma-b.com>
2019-08-14 23:17:19 +0900

    action type を value objectにする.

--- websocket/packet/packet.go
+++ websocket/packet/packet.go
@@ -3,9 +3,15 @@ package packet
 import "github.com/uenoryo/chitoi/database/row"
 
 // RequestPacket は各クライアントから送信される1回分のデータ
+const (
+	ActionTypeUndefined = iota
+	ActionTypeEntryAsHost
+	ActionTypeEntryAsGuest
+)
+
 type RequestPacket struct {
-	SessionID  string `json:"session_id"`
-	ActionType uint32 `json:"action_type"`
+	SessionID  string     `json:"session_id"`
+	ActionType ActionType `json:"action_type"`
 	SenderID   uint64
 	RoomCode   uint32
 }
@@ -15,4 +21,21 @@ type BloadcastPacket struct {
 	*RequestPacket
 	Player1 *row.User `json:"player1"`
 	Player2 *row.User `json:"player2"`
+}
+
+type ActionType int
+
+// IsUndefined ...
+func (at ActionType) IsUndefined() bool {
+	return int(at) == ActionTypeUndefined
+}
+
+// IsEntryAsHost ...
+func (at ActionType) IsEntryAsHost() bool {
+	return int(at) == ActionTypeEntryAsHost
+}
+
+// IsEntryAsGuest ...
+func (at ActionType) IsEntryAsGuest() bool {
+	return int(at) == ActionTypeEntryAsGuest
 }
