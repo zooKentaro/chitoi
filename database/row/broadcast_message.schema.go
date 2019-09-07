110a282db67e0a6f1b7b2a495ec5fe997ff8d263
uenoryo <uenoryo@uma-b.com>
2019-09-07 10:59:26 +0900

    add broadcast message.

---
+++ database/row/broadcast_message.schema.go
@@ -0,0 +1,29 @@
+package row
+
+import (
+	"time"
+
+	"github.com/kayac/ddl-maker/dialect"
+	"github.com/kayac/ddl-maker/dialect/mysql"
+)
+
+type BroadcastMessage struct {
+	ID        uint64    `ddl:"auto" json:"id,string"`
+	Title     string    `ddl:"title" json:"title"`
+	Body      string    `ddl:"body" json:"body"`
+	OpenAt    time.Time `ddl:"open_at" json:"open_at"`
+	CloseAt   time.Time `ddl:"close_at" json:"close_at"`
+	CreatedAt time.Time `db:"created_at" json:"created_at"`
+	UpdatedAt time.Time `db:"updated_at" json:"updated_at"`
+	DeletedAt time.Time `db:"deleted_at" json:"deleted_at"`
+}
+
+func (r *BroadcastMessage) PrimaryKey() dialect.PrimaryKey {
+	return mysql.AddPrimaryKey("id")
+}
+
+func (r *BroadcastMessage) Indexes() dialect.Indexes {
+	return dialect.Indexes{
+		mysql.AddIndex("open_at_close_at_idx", "open_at", "close_at"),
+	}
+}
