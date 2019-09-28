e60b65a7a72ef2d3427f8477a0eb681826abda92
uenoryo <uenoryo@uma-b.com>
2019-09-29 00:51:44 +0900

    stringタグをつける.

--- data/business_data.go
+++ data/business_data.go
@@ -1,22 +1,22 @@
 package data
 
 import (
-    "github.com/uenoryo/chitoi/database/row"
+	"github.com/uenoryo/chitoi/database/row"
 )
 
 // BusinessListResponse is XXX
 type BusinessListResponse struct {
-    Businesses []*row.Business `json:"businesses"`
+	Businesses []*row.Business `json:"businesses"`
 }
 
 // BusinessBuyRequest is XXX
 type BusinessBuyRequest struct {
-    SessionID  string `json:"session_id"`
-    BusinessID uint32 `json:"business_id"`
+	SessionID  string `json:"session_id"`
+	BusinessID uint32 `json:"business_id,string"`
 }
 
 // BusinessBuyResponse is XXX
 type BusinessBuyResponse struct {
-    UserBusinesses []*row.UserBusiness `json:"user_businesses"`
-    AfterRank      uint32              `json:"after_rank"`
+	UserBusinesses []*row.UserBusiness `json:"user_businesses"`
+	AfterRank      uint32              `json:"after_rank"`
 }
